799d51aaa51b425b1b7ec35693c5bc41ae89a001
Simon Osborne <SRC_Osborne1992@hotmail.co.uk>
2015-10-14 14:28:27 +0100

    submitting changes to person to account for book size

--- person.rb
+++ person.rb
@@ -8,7 +8,11 @@ class Person
   end
 
   def pretty_string
+    if books.size == 1
+    "#{name} has #{books.size} book borrowed from the library."
+    else
     "#{name} has #{books.size} books borrowed from the library."
+    end
   end
 
 end

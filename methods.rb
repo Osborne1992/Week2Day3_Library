f71851ca0394f06bce73249279394461433b03ac
Simon Osborne <SRC_Osborne1992@hotmail.co.uk>
2015-10-14 11:03:44 +0100

    Submitting changes to methods.rb

---
+++ methods.rb
@@ -0,0 +1,20 @@
+def menu
+  puts `clear`
+  puts "*** Hard Copy Rental - CodeClan Library ***"
+
+
+  puts
+  puts
+  puts "1. Create Book"
+  puts "2. Create Person"
+  puts "3. List All Books"
+  puts "4. List All People"
+  puts "5. Lend Book"
+  puts "6. Return Book"
+  puts "7. List Borrowed Books"
+  puts
+  puts "0. Quite"
+  puts
+  print "--> "
+  gets.to_i
+end
